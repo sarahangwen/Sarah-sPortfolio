a8c9ab99dc8dc2b696ca5909c944da4d55d97dc5
sarahangwen <angwensarahsunday@gmail.com>
2026-04-19 15:16:07 +0300

    deleted the previous cv file

--- my-react-app/src/components/Contact.tsx
+++ my-react-app/src/components/Contact.tsx
@@ -1,7 +1,7 @@
 import { Mail, MapPin, Phone, Send, CheckCircle, AlertCircle, Download } from 'lucide-react';
 import { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import resumePDF from '@/assets/Angwen-Sarah CV (2).pdf';
+import resumePDF from '@/assets/sarah-resume.pdf';
 
 const SERVICE_ID  = 'service_zbhi0m8';
 const TEMPLATE_ID = 'template_e597mwl';
